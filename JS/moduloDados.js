58f6e4d5f692f0fb2db78807852fc9e3054db832
Markdev2707 <markdesenvolvimento@gmail.com>
2022-07-08 10:10:39 -0300

    Adição sistema de modulos para execução do login

---
+++ JS/moduloDados.js
@@ -0,0 +1,28 @@
+const userLogin = [
+{
+    id: 1001,
+    userName: "Marcus",
+    senha:  "malhado123",
+    email: "markdesenvolvimento@gmail.com"
+},
+{
+    id: 1002,
+    userName: "Luana",
+    senha:  "Lua123",
+    email: "lua12@gmail.com"
+},
+{
+    id: 1001,
+    userName: "jhonny",
+    senha:  "jonh123",
+    email: "jonhjonh@gmail.com"
+},
+{
+    id: 1001,
+    userName: "elisabeth",
+    senha:  "eli123",
+    email: "elisa32@gmail.com"
+},
+];
+
+export default userLogin
